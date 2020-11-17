456cb1a22a1c5f2bea3099feaa72fb1fec77433a
TOUTA-T <toutaby@aol.com>
2020-11-17 09:15:37 +0000

    課題作成

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,10 +1,10 @@
 class Property < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :fee, presence: true
-  validates :address, presence: true, uniqueness: true
+  validates :address, presence: true
   validates :age, presence: true
   validates :remarks, presence: true
 
-  has_many :nearest_stations
-  accepts_nested_attributes_for :nearest_stations
+  has_many :nearest_stations, dependent: :destroy
+  accepts_nested_attributes_for :nearest_stations, reject_if: :all_blank
 end
